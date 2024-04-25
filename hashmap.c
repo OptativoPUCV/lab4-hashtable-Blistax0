40fb361cde64d1f54ac2ebd78b4030f30cc28035
Blistax <71483762+Blistax0@users.noreply.github.com>
2024-04-25 22:11:22 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -106,9 +106,19 @@ void eraseMap(HashMap * map,  char * key) {
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
+  if (map == NULL || key == NULL)
+    exit(EXIT_FAILURE);
 
-
-    return NULL;
+  size_t posicion = hash(key, map->capacity);
+  
+  while (map->buckets[posicion] != NULL){
+    if(is_equal(map->buckets[posicion]->key, key)){
+      map->current = posicion;
+      return map->buckets[posicion];
+    }
+    posicion = (posicion+1)%map->capacity;
+  }
+  return NULL;
 }
 
 Pair * firstMap(HashMap * map) {

c9fcbd9f9f91e4e0ab6144ddea4fe8e05fb9a8ea
Blistax <71483762+Blistax0@users.noreply.github.com>
2024-04-27 02:48:58 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -122,8 +122,17 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
+  if(map == NULL)
+    exit(EXIT_FAILURE);
 
-    return NULL;
+  for(size_t i = 0; i < map->capacity; i++){
+    if(map->buckets[i] != NULL && map->buckets[i]->key != NULL){
+      map->current = i;
+      return map->buckets[i];
+    }
+  }
+  
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
